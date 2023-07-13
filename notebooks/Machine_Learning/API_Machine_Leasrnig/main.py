e34a0e323ee6617f682a5ac05c4a21983163af40
Aljiquita <98359502+Aljiquita@users.noreply.github.com>
2023-07-13 17:52:07 -0500

    Update main.py
    jim

--- notebooks/Machine_Learning/API_Machine_Leasrnig/main.py
+++ notebooks/Machine_Learning/API_Machine_Leasrnig/main.py
@@ -1,12 +1,8 @@
 from fastapi import FastAPI
-import uvicorn
 
-uvicorn.run(app, host="0.0.0.0", port=10000)
 app = FastAPI()
 
 @app.get("/peliculas_mes/{mes}")
 def peliculas_mes(mes: str):
-    
-    
-    return {'mes':mes  }
+    return {'mes': mes}
     
