11e3e62f09ba5c2bb96f024c4535eef02027582f
Aljiquita <98359502+Aljiquita@users.noreply.github.com>
2023-07-15 15:40:25 -0500

    streamlit

--- notebooks/Machine_Learning/API.py
+++ notebooks/Machine_Learning/API.py
@@ -1,51 +1,20 @@
 import requests
-import tkinter as tk
-from tkinter import messagebox
+import streamlit as st
 
-def obtener_respuesta():
-    # Crear una ventana emergente
-    ventana = tk.Tk()
+url = "https://mlapi2.onrender.com/clasificar_sismo/7.2/6.8"
 
-    # Función para manejar el clic en el botón de enviar
-    def enviar_valores():
-        # Obtener los valores de magnitud e intensidad ingresados por el usuario
-        magnitud = float(entry_magnitud.get())
-        intensidad = float(entry_intensidad.get())
+# Valores de magnitud e intensidad que deseas enviar
+magnitud = 7.2
+intensidad = 6.8
 
-        # Construir la URL con los valores proporcionados
-        url_con_valores = f"https://mlapi2.onrender.com/clasificar_sismo/{magnitud}/{intensidad}"
+# Construir la URL con los valores proporcionados
+url_con_valores = f"https://mlapi2.onrender.com/clasificar_sismo/{magnitud}/{intensidad}"
 
-        # Realizar la solicitud GET
-        response = requests.get(url_con_valores)
+# Realizar la solicitud GET
+response = requests.get(url_con_valores)
 
-        # Obtener la respuesta del servidor
-        respuesta = response.text
+# Obtener la respuesta del servidor
+respuesta = response.text
 
-        # Mostrar la respuesta obtenida en una ventana emergente
-        messagebox.showinfo("Respuesta", respuesta)
-
-        # Cerrar la ventana después de mostrar la respuesta
-        ventana.destroy()
-
-    # Crear etiquetas y campos de entrada para los valores de magnitud e intensidad
-    label_magnitud = tk.Label(ventana, text="Magnitud:", font=("Arial", 14))
-    label_magnitud.pack()
-    entry_magnitud = tk.Entry(ventana, font=("Arial", 14))
-    entry_magnitud.pack()
-
-    label_intensidad = tk.Label(ventana, text="Intensidad:", font=("Arial", 14))
-    label_intensidad.pack()
-    entry_intensidad = tk.Entry(ventana, font=("Arial", 14))
-    entry_intensidad.pack()
-
-    # Crear botón para enviar los valores
-    boton_enviar = tk.Button(ventana, text="Enviar", command=enviar_valores, font=("Arial", 14))
-    boton_enviar.pack()
-
-    # Configurar el tamaño de la ventana de preguntas
-    ventana.geometry("500x400")
-
-    # Ejecutar la ventana
-    ventana.mainloop()
-
-obtener_respuesta()
+# Mostrar la respuesta obtenida
+print(respuesta)
